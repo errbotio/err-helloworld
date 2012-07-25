b0bd6f4e9d1fb2e9af475b220f70e94f2f0325c9
Joshua Tobin <joshuatobin@gmail.com>
2012-07-25 13:08:11 +0000

    Adding helloworld example for err plugins

---
+++ helloWorld.py
@@ -0,0 +1,8 @@
+from errbot.botplugin import BotPlugin
+from errbot.jabberbot import botcmd
+
+class HelloWorld(BotPlugin):
+    @botcmd                               # this tag this method as a command
+    def hello(self, mess, args):          # it will respond to !hello
+        """ this command says hello """   # this will be the answer of !help hello
+        return 'Hello World !'            # the bot will answer that
