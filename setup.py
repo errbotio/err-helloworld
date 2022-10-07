e2a34bc3332bd9756292f70dab376227a2f1eaf1
Sijis Aviles <sijis.aviles@gmail.com>
2022-10-07 01:15:06 -0500

    refactor: update layout to src structure

--- setup.py
+++ setup.py
@@ -2,10 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name="errbot-plugin-helloworld",
-    version="1.0",
+    version="1.1",
     description="Errbot HelloWorld plugin",
     author="Errbot",
-    packages=find_packages(),
+    packages=find_packages(where="src"),
+    package_dir={'': 'src'},
+    package_data={
+        "helloworld": [
+            "*.plug",
+        ],
+    },
     include_package_data=True,
     entry_points={
         "errbot.plugins": [
